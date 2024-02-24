fee8111563072cf0107f38379c0b61e302d09c2a
NicholasGoncharov <mcfaep@vk.com>
2024-02-24 17:20:08 +0500

    test

---
+++ components/UI/UISwitch.vue
@@ -0,0 +1,28 @@
+<template>
+    <label class="relative flex h-full max-h-6 w-full max-w-12 cursor-pointer items-center rounded-full bg-[#E9E9E9] transition-all duration-300 has-[:disabled]:cursor-default has-[:checked]:bg-[#E6B5FD]" >
+        <input
+            v-model="checked"
+            :value="checked"
+            :disabled="disabled"
+            type="checkbox"
+            class="peer sr-only"
+            @click="() => checked = !checked" />
+
+        <span
+            class="absolute z-10 m-0.5 h-5 w-5 translate-x-0 rounded-full bg-white transition-transform peer-[:checked]:translate-x-6"></span>
+    </label>
+</template>
+
+<script setup lang="ts">
+const checked = defineModel<Boolean>('checked', {
+    default: false
+})
+
+const props = defineProps({
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+})
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex justify-center items-center h-screen w-screen">
+        <UISwitch />
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
